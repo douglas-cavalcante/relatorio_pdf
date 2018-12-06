7549545834231f4e3cbfd6571ac0d58b55f0d1c9
douglas-cavalcante <henriquedouglas2803@gmail.com>
2018-12-06 16:28:47 -0300

    Adiciona projeto inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__. '/vendor/autoload.php';
+$mpdf = new \Mpdf\Mpdf(['format'=>'A4-L','orientation'=>'L']);
+//Table
+$lines =  "";
+for($i = 1 ; $i < 11; $i++){
+    $color = ($i % 2 == 0) ? '#CCC' : '#FFF';
+    $lines =  $lines."
+    <tr style='background-color:{$color};'>
+        <td>Coluna {$i}</td>
+        <td>Coluna  {$i}</td>
+        <td>Coluna  {$i}</td>
+    </tr>
+    ";
+}
+$headTable = "<table style='width:100%;'>";
+$footerTable = "</table>";
+$table = $headTable . $lines . $footerTable;
+//end table
+//Page 
+$htmlContent = "
+   <html>
+      <body>
+        
+ 
+           {$table}
+      </body>
+</html>";
+//End page
+$mpdf->WriteHTML($htmlContent);
+$mpdf->Output();
+?>
